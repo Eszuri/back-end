542f60a8d27bd0eaa566bb1b22fbe69925924a35
Eszuri <ualdo296@gmail.com>
2024-06-29 11:39:17 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,11 @@
 import "dotenv/config"
-import cluster from 'cluster';
-import { cpus } from 'os';
 import express from 'express';
 import middleware from './config/middleware';
 import mongoose from 'mongoose';
 import { api } from './controller';
 import root from "./controller/home";
 const app = express();
-const numCPUs = cpus().length;
 
-
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-
-    // Fork workers
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-        // Optionally restart the worker
-        cluster.fork();
-    });
-} else {
 
 middleware(app);
 
@@ -40,4 +22,3 @@ app.use('/api', api);
 app.listen(3000, () => {
     console.log("ExpressJs Running In http://localhost:4000");
 })
-}
